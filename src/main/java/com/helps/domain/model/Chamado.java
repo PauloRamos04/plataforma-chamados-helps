e406208c2f69f855e1f87bdc3346de574da92484
Paulinho <pauloramos.04@hotmail.com>
2025-03-19 23:52:48 -0300

    feat: add models

---
+++ src/main/java/com/helps/domain/model/Chamado.java
@@ -0,0 +1,93 @@
+package com.helps.domain.model;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+
+@Entity
+public class Chamado {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String tipo;
+    private String categoria;
+    private String status;
+    private String descricao;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Date getDataCriacao() {
+        return dataCriacao;
+    }
+
+    public void setDataCriacao(Date dataCriacao) {
+        this.dataCriacao = dataCriacao;
+    }
+
+    public Usuario getSolicitante() {
+        return solicitante;
+    }
+
+    public void setSolicitante(Usuario solicitante) {
+        this.solicitante = solicitante;
+    }
+
+    public Usuario getHelper() {
+        return helper;
+    }
+
+    public void setHelper(Usuario helper) {
+        this.helper = helper;
+    }
+
+    private Date dataCriacao;
+
+    @ManyToOne
+    @JoinColumn(name = "solicitante_id")
+    private Usuario solicitante;
+
+    @ManyToOne
+    @JoinColumn(name = "helper_id")
+    private Usuario helper;
+
+}
---
+++ src/main/java/com/helps/domain/model/Mensagem.java
@@ -0,0 +1,63 @@
+package com.helps.domain.model;
+
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+@Entity
+public class Mensagem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String mensagem;
+    private Date dataEnvio;
+
+    @ManyToOne
+    @JoinColumn(name = "chamado_id")
+    private Chamado chamado;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getMensagem() {
+        return mensagem;
+    }
+
+    public void setMensagem(String mensagem) {
+        this.mensagem = mensagem;
+    }
+
+    public Date getDataEnvio() {
+        return dataEnvio;
+    }
+
+    public void setDataEnvio(Date dataEnvio) {
+        this.dataEnvio = dataEnvio;
+    }
+
+    public Chamado getChamado() {
+        return chamado;
+    }
+
+    public void setChamado(Chamado chamado) {
+        this.chamado = chamado;
+    }
+
+    public Usuario getRemetente() {
+        return remetente;
+    }
+
+    public void setRemetente(Usuario remetente) {
+        this.remetente = remetente;
+    }
+
+    @ManyToOne
+    @JoinColumn(name = "remetente_id")
+    private Usuario remetente;
+
+}
